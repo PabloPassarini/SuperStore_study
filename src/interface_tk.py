f23cd1bc62d84adfd4ac88561198ff89d798c2ca
Pablo de Oliveira Passarini <pablo4passarini@gmail.com>
2025-07-14 10:59:16 -0300

    auto_comp

--- src/interface_tk.py
+++ src/interface_tk.py
@@ -19,6 +19,7 @@ class Main_Window(Frame):
         self.df['Moth'] = self.df['Order Date Converted'].dt.month
         self.df['Day'] = self.df['Order Date Converted'].dt.day
         self.data_interface()
+        self.ml_interface()
 
 
     def data_interface(self):
@@ -34,8 +35,62 @@ class Main_Window(Frame):
             Label(lbf_select_data, text=filtros[i], font=self.font_1).grid(row=i,column=0, sticky='WS', pady=10, padx=10)
             ttk.Combobox(lbf_select_data, textvariable=self.ent_fdata[i], values=map_filt[filtros[i]], font=self.font_1,  width=53).grid(row=i, column=1, pady=10, sticky='NEWS', padx=10)
     
-    
+    def ml_interface(self):
+        self.lbf_mkl_param = LabelFrame(self.master, text='Parâmetros do Modelo', font=self.font_1b)
+        self.lbf_mkl_param.grid(row=2, column=0, sticky='NEWS', padx=10, pady=10)
+
+        models = ['Random Forest', 'Neural Network']
+        self.model_type = StringVar(value='Random Forest')
+        self.model_type_ant = 'Neural Network'
+        Label(self.lbf_mkl_param, text='Model:', font=self.font_1).grid(row=0, column=0, sticky='NEWS', padx=10, pady=10)
+        ttk.Combobox(self.lbf_mkl_param, textvariable=self.model_type, values=models, font=self.font_1, width=30).grid(row=0, column=1, padx=10, pady=10, sticky='NEWS')
         
+        hp_param = {'Param 1': [1, 2, 3], 'Param 2': [1, 2, 3], 'Param 3': [1, 2, 3], 'Param 4': [1, 2, 3], 'Param 5': [1, 2, 3], 'Param 6': [1, 2, 3]}
+        linha, coluna, cont = 1, 0, 0
+        self.elementos = list()
+        self.cb_values = [StringVar() for _ in range(len(hp_param))]
+        for key, val in hp_param.items():
+            if coluna == 2:
+                coluna = 0
+                linha += 2
+            lb = Label(self.lbf_mkl_param, text=key, font=self.font_1)
+            lb.grid(row=linha, column=coluna, sticky='NEWS', padx=10)
+            cb = ttk.Combobox(self.lbf_mkl_param, textvariable=self.cb_values[cont], values=val, font=self.font_1, width=30)
+            cb.grid(row=linha+1, column=coluna, padx=10, pady=10, sticky='NEWS')
+            self.elementos.append([lb, cb])
+
+            coluna += 1
+            cont +=1  
+        self.refresh()
+
+
+    def hyper_param(self):
+        if self.model_type.get() == 'Random Forest':
+            hp_rf_param = {'n_estimators': [100, 200, 300], 'max_depth': [None, 10, 20, 30], 'min_samples_split': [2, 5, 10], 'min_samples_leaf': [1, 2, 4], 'max_features': ['auto', 'sqrt', 'log2'], 'bootstrap': [True, False]}
+            cont = 0
+            for key, val in hp_rf_param.items():
+                self.elementos[cont][0]['text'] = key
+                self.elementos[cont][1]['values'] = val
+                self.cb_values[cont].set('- None -') 
+                cont +=1        
+        else:
+            hp_rn_param = {'loss': ['squared_error', 'poisson'], 'activation': ['identity', 'logistic', 'tanh', 'relu'], 'solver': ['lbfgs', 'sgd', 'adam'], 'learning_rate': ['constant', 'invscaling', 'adaptive'], 'learning_rate_init': [0.1, 0.01, 0.001, 0.0001], 'n_layers': [i for i in range(1,31)]}
+            cont = 0
+            for key, val in hp_rn_param.items():
+                self.elementos[cont][0]['text'] = key
+                self.elementos[cont][1]['values'] = val
+                self.cb_values[cont].set('- None -') 
+                cont +=1   
+    
+    def refresh(self):
+        if self.model_type.get() != self.model_type_ant:
+            self.model_type_ant = self.model_type.get()
+            self.hyper_param()
+
+
+
+        self.after(250, self.refresh)
+
 
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, master=None, bg='white')
